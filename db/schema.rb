513d6d5395cc57ca85b0967a8dd5c0d7813664b3
佐々木　莉奈 <1072sasaki@gmail.com>
2022-05-08 22:50:46 +0900

    [create]postのマイグレーションファイルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_08_132317) do
+ActiveRecord::Schema.define(version: 2022_05_08_134721) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "context"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
