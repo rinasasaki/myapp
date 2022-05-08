0576b75feaf7a4c12b2cf5e011cb80c8ac06a649
佐々木　莉奈 <1072sasaki@gmail.com>
2022-05-09 00:04:27 +0900

    [add]newアクションを追加

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -4,4 +4,20 @@ class PostController < ApplicationController
     @posts = Post.all
   end
   
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(task_params）
+    if @post.save
+      redirect_to post_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def post_params
+end
 end
