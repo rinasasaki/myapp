bdad13857894951c52fe6210da0e163d4a338497
佐々木　莉奈 <1072sasaki@gmail.com>
2022-05-11 10:46:55 +0900

    [fix]　投稿のフォームを変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,6 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-  
-  def show
-  end
-
 
 def new
   @post = Post.new
@@ -13,17 +9,15 @@ end
 
 
 def create
-  @post = Post.new(post_params)
-  if @post.save
-    redirect_to post_path(@post)
-  else
-    render :new
-  end
+  @post = Post.create(post_params)
+  redirect_to posts_path
 end
 
+def show
+end
 
 private
-  def post_params
-    params.require(:post).permit(:title, :body)
-  end
+def post_params
+  params.require(:post).permit(:context)
+end
 end
