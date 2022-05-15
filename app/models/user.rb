d0444520eb2fd98f19379dc83b65e8ce407f7b6f
佐々木　莉奈 <1072sasaki@gmail.com>
2022-05-15 20:05:40 +0900

    [fix]モデルを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-<<<<<<< HEAD
-<<<<<<< HEAD
+
       :recoverable, :rememberable, :validatable
-=======
-  :recoverable, :rememberable, :validatable
-
-  has_many :posts, dependent: :destroy, foreign_key: :post_user_id
->>>>>>> e83a75146479e5d2c1b9a2e7b8e3e9e293ec9c9c
-=======
-  :recoverable, :rememberable, :validatable
-
-  has_many :posts, dependent: :destroy, foreign_key: :post_user_id
->>>>>>> e83a75146479e5d2c1b9a2e7b8e3e9e293ec9c9c
 end
 
 
