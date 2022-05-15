4a32f5674ae78305e0308c594fbe36899d07132f
佐々木　莉奈 <1072sasaki@gmail.com>
2022-05-15 20:22:36 +0900

    [add]　ログイン画面のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
+  get "login" => "users#login_form"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
