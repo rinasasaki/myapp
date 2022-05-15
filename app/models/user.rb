d5a2d6a6b1975e58563eea2e2abc68d65cddf582
佐々木　莉奈 <1072sasaki@gmail.com>
2022-05-15 14:40:37 +0900

    [fix]

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
+<<<<<<< HEAD
       :recoverable, :rememberable, :validatable
+=======
+  :recoverable, :rememberable, :validatable
+
+  has_many :posts, dependent: :destroy, foreign_key: :post_user_id
+>>>>>>> e83a75146479e5d2c1b9a2e7b8e3e9e293ec9c9c
 end
