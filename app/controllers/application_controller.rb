cd3a28a613773e11ed41718baefafc2c44446ac8
佐々木　莉奈 <1072sasaki@gmail.com>
2022-05-17 00:18:34 +0900

    [add] 名前のフォームを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-  
+
+  before_action :authenticate_user!
+
+def after_sign_in_path_for(resource)
+  posts_root_path 
+end
+
+def after_sign_out_path_for(resource)
+  new_user_session_path 
+end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,3 @@
 def login_form
 end
+
