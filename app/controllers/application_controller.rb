fb95392f372976e131db2afc843d2bcb471b4110
佐々木　莉奈 <1072sasaki@gmail.com>
2022-05-17 00:23:55 +0900

    [fix]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,12 @@ end
 def after_sign_out_path_for(resource)
   new_user_session_path 
 end
+
+before_action :configure_permitted_parameters, if: :devise_controller? 
+
+private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:email]) 
+  end
 end
