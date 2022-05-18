3725423db6136f474cd2e765fa9fcd1439d869eb
佐々木　莉奈 <1072sasaki@gmail.com>
2022-05-18 23:45:19 +0900

    [create]　投稿機能を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,1 @@
-def login_form
-end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
-  belongs_to:user
+  belongs_to :user
 
-def user
-  return User.find_by(id: self.user_id)
-end
+  def user
+    return User.find_by(id: self.user_id)
+  end
+
 end
