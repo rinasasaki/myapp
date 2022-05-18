48745c8dc2ecf3a78cb89e4c0e80ca6989be6e89
佐々木　莉奈 <1072sasaki@gmail.com>
2022-05-18 22:58:10 +0900

    [fix]名前を登録できるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,19 @@
 class ApplicationController < ActionController::Base
 
+  before_action :configure_permitted_parameters, if: :devise_controller? 
   before_action :authenticate_user!
 
-def after_sign_in_path_for(resource)
-  posts_root_path 
-end
-
-def after_sign_out_path_for(resource)
-  new_user_session_path 
-end
-
-before_action :configure_permitted_parameters, if: :devise_controller? 
-
-private
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,keys:[:email]) 
+  def after_sign_in_path_for(resource)
+    posts_path 
   end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path 
+  end
+
+  private
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:name]) 
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :posts
   devise_for :users
-
-  get "login" => "users#login_form"
 end
+
