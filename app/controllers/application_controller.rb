fe0738e13bde69fd143e448d7bc9488b541f79c8
佐々木　莉奈 <1072sasaki@gmail.com>
2022-05-22 02:57:29 +0900

    [add]ログアウトボタンを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class ApplicationController < ActionController::Base
     new_user_session_path 
   end
 
+
+
   private
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up,keys:[:name]) 
