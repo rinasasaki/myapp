308d5a4906ee89fb8f0cbdd58c3e9debf09bde34
佐々木　莉奈 <1072sasaki@gmail.com>
2022-06-17 14:09:25 +0900

    [add] destroyアクションを追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,16 +3,18 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(event_params)
     @event.user_id = current_user.id
-    if @post.save
-      redirect_to posts_path
+    if @event.save
+      redirect_to events_path
     else
       render 'new'
     end
   end
 
-  def delete 
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
   end
-end
 
 private
   def event_params
