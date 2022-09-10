e1b27d572bbdad7b20b23144b4d180d597a5071d
佐々木　莉奈 <1072sasaki@gmail.com>
2022-09-11 00:00:04 +0900

    [fix]ルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # get '/users/:id', to: 'user#show'
-  # patch'/users/:id', to: 'user#update'
-  resources :users, only: [:show, :update, :edit]
+  get '/users/:id', to: 'user#show'
+  patch'/users/:id', to: 'user#update'
+  resources :users, only: [:show, :update, :edit] 
   resources :posts
   resources :events
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_17_045453) do
+ActiveRecord::Schema.define(version: 2022_07_08_105655) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
